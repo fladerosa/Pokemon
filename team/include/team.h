0129bbe8d7b449872a0f1e8297b0bcfa85433646
fladerosa <fderosa@est.frba.utn.edu.ar>
2020-04-15 15:49:37 -0300

    problemas para agregar otros .h y .c, por el momento se unifica codigo

--- team/include/team.h
+++ team/include/team.h
@@ -3,8 +3,30 @@
 
 #include "common_utils.h"
 
-enum t_algoritmo {fifo, rr, sjf_sd, sjf_cd};   
+typedef struct configuracion{
+       char *ip_team;
+       uint32_t *puerto_team;
+       t_list *posicion_entrenador;
+       t_list *pokemon_entrenador;
+       t_list *objetivo_entrenador
+       uint32_t  tiempo_reconexion; 
+       uint32_t  retardo_ciclo_cpu; 
+       char *algoritmo_planificacion;
+       uint32_t quantum;
+       uint32_t estimacion_inicial;
+       char *ip_broker;
+       uint32_t *puerto_broker;       
 
+}configuracion;
+
+configuracion valores;
+t_config * leer_config();
+t_logger * crear_logger();
+void cargar_valores_config();
+void inicializar();
+void liberar_recursos();   
 void establecerConexionGameBoy();
 void establecerConexionBroker();
+enum t_algoritmo {fifo, rr, sjf_sd, sjf_cd};
+
 #endif
--- team/src/team.c
+++ team/src/team.c
@@ -1,13 +1,9 @@
 #include "team.h"
-#include "configuracion.h" 
 
 int main(int argc, char ** argv){
     
-//crear t_list de las commons que usa la estructura configuracion 
-//REVISAR
-    t_list *posicion_entrenador = list_create();
-    t_list *pokemon_entrenador = list_create();
-    t_list *objetivo_entrenador = list_create();
+t_config *config;
+t_logger * logger;
     
     inicializar();
 
@@ -31,4 +27,86 @@ void establecerConexionBroker()
 {
     int conexion = crear_conexion(valores.ip_broker, valores.puerto_broker);
     enviar_mensaje("Prueba", conexion);
+}
+
+//inicializo listas y valores del archivo de configuracion
+t_config * leer_config()
+{   
+    char* config_path = "./cfg/team.config";
+    config = config_create(config_path); 
+    if(config == NULL)   
+    {
+        error_show("Error al leer el config del proceso TEAM en %s\n", config_path);
+        exit(LOG_FAIL);
+    }
+             
+    return  config;
+}
+
+void cargar_valores_config()
+{
+    valores.ip_team = config_get_string_value(config, "IP_TEAM");
+    valores.puerto_team = config_get_int_value(config, "PUERTO_TEAM");
+    valores.tiempo_reconexion = config_get_int_value(config, "TIEMPO_RECONEXION");
+    valores.retardo_ciclo_cpu = config_get_int_value(config, "RETARDO_CICLO_CPU");
+    valores.algoritmo_planificacion = config_get_string_value(config, "ALGORITMO_PLANIFICACION");
+    valores.quantum = config_get_int_value(config, "QUANTUM");
+    valores.estimacion_inicial = config_get_int_value(config, "ESTIMACION_INICIAL");
+    valores.ip_broker = config_get_string_value(config, "IP_BROKER");
+    valores.puerto_broker= config_get_int_value(config, "PUERTO_BROKER");
+}
+
+t_logger * crear_logger()
+{      
+    char* log_config = config_get_string_value(config, "LOG_FILE");
+    t_logger * logger = log_create(log_config, "TEAM", 1, LOG_LEVEL_INFO);
+    if(logger == NULL)
+    {
+        error_show("Error al crear el logger del proceso TEAM en %s\n", log_config);
+        exit(LOG_FAIL);
+    }
+    return logger;
+}
+ 
+void inicializar()
+{    
+    config = leer_config();
+
+    logger = crear_logger();
+    log_info(logger, "Log creado con exito\n", LOG_LEVEL_INFO);
+    cargar_valores_config();
+    log_info(logger, "Inicializacion y carga de configuracion exitosa\n", LOG_LEVEL_INFO);
+}
+
+
+void liberar_recursos()
+{
+   
+    if(config)
+    {
+        config_destroy(config);
+    }
+
+    if(logger)
+    {
+        log_destroy(logger);
+    }
+    
+    stop_server();
+    close(conexion);
+}
+
+enum t_algoritmo obtenerAlgoritmo()
+{
+	enum t_algoritmo esAlgoritmo;
+	char* sAlgort = valores.algoritmo_planificacion;
+    if(string_equals_ignore_case(sAlgort, "FIFO"))
+		esAlgoritmo = fifo;
+	else if(string_equals_ignore_case(sAlgort, "RR"))
+		esAlgoritmo = rr;
+	else if(string_equals_ignore_case(sAlgort, "SJF-SD"))
+		esAlgoritmo = sjf_sd;
+	else if(string_equals_ignore_case(sAlgort, "SJF-SD"))
+		esAlgoritmo = sjf_cd;
+	return esAlgoritmo;
 }
