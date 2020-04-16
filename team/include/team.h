805134ff04cb39ea4447305732781a04e9ec5e55
fladerosa <fderosa@est.frba.utn.edu.ar>
2020-04-16 18:00:31 -0300

    modificacion de funciones

--- team/include/team.h
+++ team/include/team.h
@@ -1,6 +1,7 @@
 #ifndef TEAM_H
 #define TEAM_H
 
+#include<commons/collections/list.h>
 #include "common_utils.h"
 
 typedef struct configuracion{
@@ -8,7 +9,7 @@ typedef struct configuracion{
        uint32_t *puerto_team;
        t_list *posicion_entrenador;
        t_list *pokemon_entrenador;
-       t_list *objetivo_entrenador
+       t_list *objetivo_entrenador;
        uint32_t  tiempo_reconexion; 
        uint32_t  retardo_ciclo_cpu; 
        char *algoritmo_planificacion;
@@ -20,8 +21,9 @@ typedef struct configuracion{
 }configuracion;
 
 configuracion valores;
-t_config * leer_config();
-t_logger * crear_logger();
+void leer_config();
+void crear_logger_obligatorio();
+void crear_logger_opcional();
 void cargar_valores_config();
 void inicializar();
 void liberar_recursos();   
--- team/src/team.c
+++ team/src/team.c
@@ -2,25 +2,22 @@
 
 int main(int argc, char ** argv){
     
-t_config *config;
-t_logger * logger;
-    
     inicializar();
 
     //establecer conexion con broker como cliente
-    establecerConexionBroker();
+    //establecerConexionBroker();
     
     //establecer puerto escucha con gameboy como servidor
-    establecerConexionGAmeBoy();
+    //establecerConexionGAmeBoy();
     
     liberar_recursos();
 
     return EXIT_SUCCESS;
 }
 
-void establecerConexionGameBoy()
+/*void establecerConexionGameBoy()
 {
-    start_server(valores.ip_team, valores.puerto_team, p_on_request);
+   start_server(valores.ip_team, valores.puerto_team, p_on_request);
 }
 
 void establecerConexionBroker()
@@ -33,54 +30,76 @@ void stop_server(int socketServer)
 {
     close(socketServer);
 }
-
-t_config * leer_config()
+*/
+void leer_config()
 {   
     char* config_path = "./cfg/team.config";
     config = config_create(config_path); 
     if(config == NULL)   
     {
         error_show("Error al leer el config del proceso TEAM en %s\n", config_path);
-        exit(LOG_FAIL);
+        exit(CONFIG_FAIL);
     }           
-    return  config;
+}
+void cargar_posiciones_team()
+{   
+    valores.posicion_entrenador = config_get_array_value(config, "POSICIONES_ENTRENADORES");
+}
+void cargar_pokemones_team()
+{
+    valores.pokemon_entrenador = config_get_array_value(config, "POKEMON_ENTRENADORES");
+}
+void cargar_objetivo_team()
+{
+    valores.objetivo_entrenador = config_get_array_value(config, "OBJETIVOS_ENTRENADORES");
 }
 
 void cargar_valores_config(t_config * config)
 {
-    valores.posicion_entrenador = config_get_array_value(config, "POSICIONES_ENTRENADORES");
-    valores.pokemon_entrenador = config_get_array_value(config, "POKEMON_ENTRENADORES");
-    valores.objetivo_entrenador = config_get_array_value(config, "OBJETIVOS_ENTRENADORES");
-    valores.tiempo_reconexion = config_get_int_value(config, "TIEMPO_RECONEXION");
-    valores.retardo_ciclo_cpu = config_get_int_value(config, "RETARDO_CICLO_CPU");
+   // valores.posicion_entrenador = config_get_array_value(config, "POSICIONES_ENTRENADORES");
+    //valores.pokemon_entrenador = config_get_array_value(config, "POKEMON_ENTRENADORES");
+   // valores.objetivo_entrenador = config_get_array_value(config, "OBJETIVOS_ENTRENADORES");
+    valores.tiempo_reconexion = (uint32_t)config_get_int_value(config, "TIEMPO_RECONEXION");
+    valores.retardo_ciclo_cpu = (uint32_t)config_get_int_value(config, "RETARDO_CICLO_CPU");
     valores.algoritmo_planificacion = config_get_string_value(config, "ALGORITMO_PLANIFICACION");
-    valores.quantum = config_get_int_value(config, "QUANTUM");
-    valores.estimacion_inicial = config_get_int_value(config, "ESTIMACION_INICIAL");
+    valores.quantum = (uint32_t)config_get_int_value(config, "QUANTUM");
+    valores.estimacion_inicial = (uint32_t)config_get_int_value(config, "ESTIMACION_INICIAL");
     valores.ip_broker = config_get_string_value(config, "IP_BROKER");
     valores.puerto_broker= config_get_int_value(config, "PUERTO_BROKER");
     valores.ip_team = config_get_string_value(config, "IP_TEAM");
     valores.puerto_team = config_get_int_value(config, "PUERTO_TEAM");
 }
 
-t_logger * crear_logger()
+void iniciar_logger_obligatorio()
 {      
     char* log_config = config_get_string_value(config, "LOG_FILE");
-    t_logger * logger = log_create(log_config, "TEAM", 1, LOG_LEVEL_INFO);
-    if(logger == NULL)
+    obligatory_logger = log_create(log_config, "TEAM", 1, LOG_LEVEL_INFO);
+    if(obligatory_logger == NULL)
     {
         error_show("Error al crear el logger del proceso TEAM en %s\n", log_config);
         exit(LOG_FAIL);
     }
-    return logger;
+    log_info(obligatory_logger, "Log Obligatorio creado correctamente\n");
 }
  
+ void iniciar_logger_opcional()
+{      
+    char* log_config = config_get_string_value(config, "LOG_FILE_OPTIONAL");
+    optional_logger = log_create(log_config, "TEAM", 1, LOG_LEVEL_INFO);
+    if(optional_logger == NULL)
+    {
+        error_show("Error al crear el logger del proceso TEAM en %s\n", log_config);
+        exit(LOG_FAIL);
+    }
+    log_info(optional_logger, "Log Opcional creado correctamente\n");
+}
 void inicializar()
 {    
-    config = leer_config();
-    logger = crear_logger();
-    log_info(logger, "Log creado con exito\n", LOG_LEVEL_INFO);
+    leer_config();
+    iniciar_logger_obligatorio();
+    iniciar_logger_opcional();
     cargar_valores_config(config);
-    log_info(logger, "Inicializacion y carga de configuracion exitosa\n", LOG_LEVEL_INFO);
+    log_info(optional_logger, "Inicializacion y carga de configuracion exitosa\n", LOG_LEVEL_INFO);
 }
 
 void liberar_recursos()
@@ -88,11 +107,14 @@ void liberar_recursos()
     if(config)
         config_destroy(config);
 
-    if(logger)
-        log_destroy(logger);
+    if(obligatory_logger)
+        log_destroy(obligatory_logger);
+
+    if(optional_logger)
+        log_destroy(optional_logger);
      
-    stop_server(socketServer);
-    close(conexion);
+  //  stop_server(socketServer);
+   // close(conexion);
 }
 
 enum t_algoritmo obtenerAlgoritmo()
