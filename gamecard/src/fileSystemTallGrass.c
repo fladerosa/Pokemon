2882b1f3f925b3b43587d0ce5de3daaaf2734721
ofasciolo <ornella_sofia@hotmail.com>
2020-07-05 13:39:18 -0300

    Bitmap

--- gamecard/src/fileSystemTallGrass.c
+++ gamecard/src/fileSystemTallGrass.c
@@ -2,8 +2,14 @@
 
 void iniciarTallGrass(){
     obtenerConfig();
-    crearBloques();
+    //crearBloques();
+    if(hayBitmap()){
+        printf("\nCreando Bitmap...\n");
+        crearArchivoBitmap();
+        printf("Bitmap Creado con exito.\n");
+    }
 
+    crearBitMap();
 }
 
 void crearBloques(){
@@ -31,7 +37,7 @@ void crearBloques(){
 
 void obtenerConfig(){
     FILE* file; 
-    char buffer[25]; //revisar
+    char buffer[25];
     char BLOCKS[4];
     char BLOCKSIZE[2];
     uint32_t j=0;
@@ -60,4 +66,91 @@ void obtenerConfig(){
     }
     configM.blocks = atoi(BLOCKS);
     fclose(file);
+}
+
+void crearArchivoBitmap(){
+
+	int blockNum = configM.blocks;
+	while(blockNum%8!=0){
+		blockNum++;
+	}
+	int bytes = BIT_CHAR(blockNum);
+	char* bitsVacios = string_repeat(0,bytes);
+
+	FILE *fp;
+	t_bitarray* arrayCreador = bitarray_create_with_mode(bitsVacios,bytes,0);
+
+	printf("\n");
+	fp=fopen("./TALL_GRASS/Metadata/Bitmap.bin","w");
+	fwrite(arrayCreador->bitarray,1,bytes,fp);
+	fclose(fp);
+
+	bitarray_destroy(arrayCreador); 
+
+	free(bitsVacios);
+}
+
+void imprimirBITARRAY(t_bitarray* bitarray){
+
+	int blockNum = configM.blocks;
+
+	for (int i = 0; i<blockNum;i++){
+		printf("%d",bitarray_test_bit(bitarray,i));
+	}
+	printf("\n");
+
+}
+
+
+void crearBitMap(){
+
+	int blockNum = configM.blocks;
+	while(blockNum%8!=0){
+			blockNum++; 
+	}
+	int bytes = BIT_CHAR(blockNum);
+	char* archivoBitmap;
+	int fd = open("./TALL_GRASS/Metadata/Bitmap.bin", O_RDWR , (mode_t)0600);
+	struct stat mystat;
+	if(fstat(fd,&mystat)<0){
+		printf("Error al establecer fstat\n");
+		close (fd);
+	}
+
+	archivoBitmap=(char*) mmap(0,mystat.st_size, PROT_READ|PROT_WRITE, MAP_SHARED ,fd,0);
+	if(archivoBitmap==MAP_FAILED){
+		printf("ERROR AL MAPEAR A MEMORIA");
+		close(fd);
+
+	}
+
+	bitmap=bitarray_create_with_mode(archivoBitmap , mystat.st_size ,0);
+
+	msync(archivoBitmap,bytes,MS_SYNC);
+
+    //imprimirBITARRAY(bitmap);
+
+	close(fd);
+}
+
+
+void ActualizarBitmap(){
+
+	int blockNum = configM.blocks;
+	while(blockNum%8!=0){
+			blockNum++; 
+	}
+	int bytes = BIT_CHAR(blockNum);
+	FILE *fp;
+	fp=fopen("./TALL_GRASS/Metadata/Bitmap.bin","w");
+	fwrite(bitmap->bitarray,1,bytes,fp);
+
+
+	fclose(fp);
+}
+
+int hayBitmap(){
+
+    return access( "./TALL_GRASS/Metadata/Bitmap.bin", F_OK ) != -1;
+
 }
