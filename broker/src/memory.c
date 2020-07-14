5a450248bbf881b0f791daab5581bd2f3e398c95
Darío <dariokozicki@gmail.com>
2020-07-13 21:06:59 -0300

    Add 0 initializers for id and id correlational

--- broker/src/memory.c
+++ broker/src/memory.c
@@ -19,7 +19,8 @@ void initializeMemory(){
     data->partition_size = data->size;
     data->offset = 0;
     data->state = FREE;
-
+    data->id = 0;
+    data->id_correlational = 0;
     list_add(memory.partitions, data);
 }
 
@@ -312,6 +313,8 @@ void BS_allocateData(uint32_t sizeData, t_data* freePartitionData){
         newData->partition_size = freePartitionData->partition_size;
         newData->offset = freePartitionData->offset + freePartitionData->partition_size;
         newData->state = FREE;
+        newData->id = 0;
+        newData->id_correlational = 0;
         pthread_mutex_lock(memory.m_partitions_modify);
         list_add(memory.partitions, newData);
         pthread_mutex_unlock(memory.m_partitions_modify);
@@ -320,6 +323,8 @@ void BS_allocateData(uint32_t sizeData, t_data* freePartitionData){
         freePartitionData->creationTime = timestamp();
         freePartitionData->lastTimeUsed = freePartitionData->creationTime;
         freePartitionData->state = USING;
+        freePartitionData->id = 0;
+        freePartitionData->id_correlational = 0;
         pthread_mutex_lock(memory.m_partitions_modify);
         list_sort(memory.partitions, _offsetAscending);
         pthread_mutex_unlock(memory.m_partitions_modify);
@@ -335,6 +340,8 @@ void DP_allocateData(uint32_t sizeData, t_data* freePartitionData){
         newData->size = newData->partition_size;
         newData->offset = freePartitionData->offset + sizeData;
         newData->state = FREE;
+        newData->id = 0;
+        newData->id_correlational = 0;
         pthread_mutex_lock(memory.m_partitions_modify);
         list_add(memory.partitions, newData);
         list_sort(memory.partitions, _offsetAscending);
@@ -342,6 +349,8 @@ void DP_allocateData(uint32_t sizeData, t_data* freePartitionData){
     }
     freePartitionData->partition_size = sizeData;
     freePartitionData->size = sizeData;
+    freePartitionData->id = 0;
+    freePartitionData->id_correlational = 0;
     freePartitionData->creationTime = timestamp();
     freePartitionData->lastTimeUsed = freePartitionData->creationTime;
     freePartitionData->state = USING;
